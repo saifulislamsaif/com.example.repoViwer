a57bdde218025aea0d97040d29a03cf5ef055bb6
saifb <saifulislamsaifsarker@gmail.com>
2024-03-22 20:18:40 +0600

    [F-> binding added P-> MainActivity ]

--- app/src/main/java/com/example/repoViwer/view/MainActivity.kt
+++ app/src/main/java/com/example/repoViwer/view/MainActivity.kt
@@ -1,12 +1,21 @@
 package com.example.repoViwer.view
 
+import android.app.Activity
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.example.repoViwer.R
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import com.example.repoViwer.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var activity: Activity
+    private lateinit var navHostFragment: NavHostFragment
+    private lateinit var navController: NavController
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
