d5e55c3b7a70b67d26024fd79a3ce4a2502b443d
saifb <saifulislamsaifsarker@gmail.com>
2024-03-22 18:37:28 +0600

    require library imported

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,16 +1,20 @@
+
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
+    id ("kotlin-kapt")
+    id ("dagger.hilt.android.plugin")
+    id("androidx.navigation.safeargs.kotlin")
 }
 
 android {
     namespace = "com.example.repoViwer"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.example.repoViwer"
-        minSdk = 24
-        targetSdk = 33
+        minSdk = 22
+        targetSdk = 34
         versionCode = 1
         versionName = "1.0"
 
@@ -33,15 +37,80 @@ android {
     kotlinOptions {
         jvmTarget = "1.8"
     }
+    buildFeatures {
+        dataBinding = true
+        viewBinding = true
+    }
 }
 
 dependencies {
-
-    implementation("androidx.core:core-ktx:1.9.0")
+    implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
-    implementation("com.google.android.material:material:1.9.0")
+    implementation("com.google.android.material:material:1.11.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+
+
+    implementation ("androidx.navigation:navigation-fragment-ktx:2.7.7")
+    implementation ("androidx.navigation:navigation-ui-ktx:2.7.7")
+
+    implementation ("androidx.activity:activity-ktx:1.8.2")
+    implementation ("androidx.fragment:fragment-ktx:1.6.2")
+
+    implementation ("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.7.3")
+    implementation ("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.7.3")
+    implementation("com.google.android.gms:play-services-vision:20.1.3")
+    implementation("com.google.android.gms:play-services-vision-common:19.1.3")
+    implementation("androidx.camera:camera-lifecycle:1.3.2")
+    implementation("androidx.camera:camera-core:1.3.2")
+    implementation("androidx.activity:activity:1.8.0")
+    implementation("androidx.camera:camera-view:1.3.2")
+
+
+    //Hilt
+    val hiltVersion="2.50"
+
+    implementation ("com.google.dagger:hilt-android:${hiltVersion}")
+    kapt ("com.google.dagger:hilt-android-compiler:${hiltVersion}")
+    kapt ("androidx.hilt:hilt-compiler:1.2.0")
+
+    // For instrumentation tests
+    androidTestImplementation("com.google.dagger:hilt-android-testing:${hiltVersion}")
+    kaptAndroidTest("com.google.dagger:hilt-compiler:${hiltVersion}")
+
+    // For local unit tests
+    testImplementation("com.google.dagger:hilt-android-testing:${hiltVersion}")
+    kaptTest("com.google.dagger:hilt-compiler:${hiltVersion}")
+
+
+    // Room
+    implementation ("androidx.room:room-runtime:2.6.1")
+    kapt ("androidx.room:room-compiler:2.6.1")
+    implementation ("androidx.room:room-ktx:2.6.1")
+
+    // Retrofit
+    implementation ("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation ("com.squareup.retrofit2:converter-gson:2.9.0")
+    implementation ("com.squareup.okhttp3:okhttp:5.0.0-alpha.2")
+    implementation ("com.squareup.okhttp3:logging-interceptor:5.0.0-alpha.2")
+    implementation ("com.squareup.retrofit2:adapter-rxjava2:2.7.2")
+
+    //Glide
+    implementation ("com.github.bumptech.glide:glide:4.12.0")
+    annotationProcessor ("com.github.bumptech.glide:compiler:4.12.0")
+
+    val nav_version = "2.7.7"
+
+
+    // Kotlin
+    implementation("androidx.navigation:navigation-fragment-ktx:$nav_version")
+    implementation("androidx.navigation:navigation-ui-ktx:$nav_version")
+
+    // Testing Navigation
+    androidTestImplementation("androidx.navigation:navigation-testing:$nav_version")
+
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
 }
+
+
--- app/src/main/java/com/example/repoViwer/view/MainActivity.kt
+++ app/src/main/java/com/example/repoViwer/view/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.example.repoViwer
+package com.example.repoViwer.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.repoViwer.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
